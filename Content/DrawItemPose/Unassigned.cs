4177ec290bc585931a5142bcf51be55246dc83da
Flashkirby <flashkirby@gmail.com>
2025-06-17 10:43:18 +0100

    2.0.1

--- Content/DrawItemPose/Unassigned.cs
+++ Content/DrawItemPose/Unassigned.cs
@@ -18,12 +18,14 @@ namespace WeaponOutLite.Content.DrawItemPose
         public virtual int UpdateIdleBodyFrame(Player p, Item i, int bodyFrame, int timer) => bodyFrame;
 
         public DrawData CalculateDrawData(DrawData data, Player p, float height, float width, int bodyFrame, int timer) {
-            // This shouldn't happen in normal gameplay - but is a known bug in MP (nosync)
-            // For now replicate None
+            //// This shouldn't happen in normal gameplay - but is a known bug in MP:
+            //// Seems to occur when two clients are running a v1 and a v2 of the mod, 
+            //// due to the change in mod type.
+            //// When this happens, hide the item.
             data.color = Color.Transparent;
             return data;
 
-
+            /*
             // Should never see this in normal conditions - useful for debugging though
             float x = (Main.mouseX - Main.screenWidth / 2);
             float y = (Main.mouseY - Main.screenHeight / 2);
@@ -35,6 +37,7 @@ namespace WeaponOutLite.Content.DrawItemPose
                 data.rotation = (float)(Main.GlobalTimeWrappedHourly * 2f) * p.direction * p.gravDir;
             }
             return data;
+            */
         }
     }
 }
